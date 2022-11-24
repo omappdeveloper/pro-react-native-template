2f006eca8cbace7b72edbe42e820b87514e48a40
omappdeveloper <omappdeveloper@gmail.com>
2022-11-24 23:16:46 +0530

    multi env and android flavor

--- template/src/screens/DemoScreen.js
+++ template/src/screens/DemoScreen.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Config from "react-native-config";
+
 export const ProfileScreen = ({ navigation }) => {
     return (
         <View style={styles.center}>
@@ -30,6 +32,7 @@ export const HomeScreen = ({ navigation }) => {
     return (
         <View style={styles.center}>
             <Text>Welcome to Professional React Native Template!</Text>
+            <Text>{Config.ENV}</Text>
             <MaterialCommunityIcons name='home' size={24} />
             <View style={{ paddingTop: 20 }}>
                 <Button
